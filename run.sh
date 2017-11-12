64cc0884ab1c5dc5d38ab9236d5c0bd93cfdd54d
Florin Peter <github@florin-peter.de>
2017-11-12 22:58:32 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-python elastalert/elastalert.py --debug --config config/config.yaml
+export $ELASTALERT_CONFIG=${ELASTALERT_CONFIG:-config/config.yaml}
+export $ELASTALERT_PARAMETER=${ELASTALERT_PARAMETER:-}
+python elastalert/elastalert.py --config $ELASTALERT_CONFIG $ELASTALERT_PARAMETER
