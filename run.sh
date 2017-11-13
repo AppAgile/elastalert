09d168814aee8175a42bfc55ac65b2e9b355a5f4
Florin Peter <github@florin-peter.de>
2017-11-13 14:37:21 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 export ELASTALERT_CONFIG=${ELASTALERT_CONFIG:-config/config.yaml}
 export ELASTALERT_PARAMETER=${ELASTALERT_PARAMETER:-''}
-python elastalert/elastalert.py --config $ELASTALERT_CONFIG $ELASTALERT_PARAMETER
+
+python elastalert/create_index.py --config config/config.yaml --index .kibana
+
+exec python elastalert/elastalert.py --config $ELASTALERT_CONFIG $ELASTALERT_PARAMETER
